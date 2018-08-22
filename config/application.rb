707747ef5a8b27b9451849787cdc0084c7d899fd
James Newman <jamesmnewman1@gmail.com>
2018-08-22 14:15:52 -0700

    adding put method

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module RedditBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options, :delete]
+        resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put]
       end
     end
   end
